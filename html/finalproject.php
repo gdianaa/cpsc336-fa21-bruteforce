e3747dd905f12fcb006b25e15a3becaa545e67b9
student <hannah.karas.19@cnu.edu>
2021-11-18 13:55:09 -0500

    creating git ignore

---
+++ html/finalproject.php
@@ -0,0 +1,11 @@
+<?php
+echo "<html><body><center>
+   <form action=\"finalproject.php\" method=\"post\"><br>
+   Item ID:<br> <input type=\"text\" name=\"itemid\"><br>
+   Item Name:<br><input type=\"text\" name=\"itemname\"><br>
+   Item Quantity:<br> <input type=\"text\" name=\"itemquantity\"> <p>
+   <input type=\"submit\" value=\"Add Item\"></form>";
+
+
+
+?>	
