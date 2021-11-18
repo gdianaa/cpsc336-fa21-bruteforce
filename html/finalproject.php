d215f94e0279948bef37e6cbb70da957b14d70d3
student <hannah.karas.19@cnu.edu>
2021-11-18 14:41:19 -0500

    added html and removed php stuff

--- html/finalproject.php
+++ html/finalproject.php
@@ -1,11 +1,13 @@
-<?php
-echo "<html><body><center>
-   <form action=\"finalproject.php\" method=\"post\"><br>
-   Item ID:<br> <input type=\"text\" name=\"itemid\"><br>
-   Item Name:<br><input type=\"text\" name=\"itemname\"><br>
-   Item Quantity:<br> <input type=\"text\" name=\"itemquantity\"> <p>
-   <input type=\"submit\" value=\"Add Item\"></form>";
 
+<html><body><center>
+   <form action="finalproject.php" method="post">
+   	<input type="text" name="id" minlength="12" maxlength="12" placeholder="item id"><br><br>
+	<input type="text" name="name" placeholder="item name"><br><br>
+	<input type="number" name="quantity" min="0" placeholder="item quantity"><br><br>
+	
+	<input type="submit" name="insert" value="Add">
+	<input type="submit" name="delete" value="Delete">
+	<input type="submit" name="search" value="Find">
 
-
-?>	
+</form>
+		
